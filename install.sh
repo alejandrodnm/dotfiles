275c0c5d0ab7a854640c4e200a5fafe2877a4ea9
Alejandro Do Nascimento <alejandrodnm@gmail.com>
2018-07-20 23:00:26 +0200

    Add powerline config

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ ln -s "$(pwd)/psqlrc" "${HOME}/.psqlrc"
 ln -s "$(pwd)/tmux.conf" "${HOME}/.tmux.conf"
 mkdir -p "${HOME}/.config/nvim"
 ln -s "$(pwd)/vimrc" "${HOME}/.config/nvim/init.vim"
+ln -s "$(pwd)/powerline" "${HOME}/.config/"
 # https://medium.com/@dubistkomisch/how-to-actually-get-italics-and-true-colour-to-work-in-iterm-tmux-vim-9ebe55ebc2be
 tic -x iterm/xterm-256color-italic.terminfo
 tic -x iterm/tmux-256color.terminfo
